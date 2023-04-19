c93dab2efcba2ad340ff14e72271da53353b5347
Andres Rosales <59161981+AndresRP645@users.noreply.github.com>
2023-04-19 13:12:06 -0600

    cambios esteticos

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -25,7 +25,7 @@ const Project = (props) => {
 export default Project;
 
 const Container = styled.div`
-  height: 250px;
+  height: 230px;
   width: 360px;
   background-color: #4e5156;
   margin: 0 0.5rem;
